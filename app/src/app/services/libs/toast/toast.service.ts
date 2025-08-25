203f191d3b1f3c37264dbd51ad1215405754bf03
Gabriel Damico <gabrieldamico22@gmail.com>
2025-08-25 19:19:23 -0300

    feat: Adiciona interceptador de erros HTTP

--- app/src/app/services/libs/toast/toast.service.ts
+++ app/src/app/services/libs/toast/toast.service.ts
@@ -8,10 +8,13 @@ export class ToastService {
   constructor(private toastr: ToastrService) {}
 
   sucesso(mensagem: string) {
-    this.toastr.success(mensagem);
+    return this.toastr.success(mensagem);
   }
 
-  erro(mensagem: string) {
-    this.toastr.error(mensagem);
+  erro(mensagem: string, descricao?: string) {
+    if (descricao) {
+      return this.toastr.error(descricao, mensagem);
+    }
+    return this.toastr.error(mensagem);
   }
 }
